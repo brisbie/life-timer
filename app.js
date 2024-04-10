6bf1192a25c6a7fb8de637780869e9fdca544898
Evan Brisbin <158214092+brisbie@users.noreply.github.com>
2024-04-10 15:05:16 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+document.getElementById("submitButn").addEventListener("click", function() {
+    hideDob();
+    calculateAge();
+});
+function hideDob(){
+    //Hide the input form
+    document.getElementById("dobForm").style.display="none";
+    document.getElementById("age").style.display="block";
+    document.getElementById("timeLeft").style.display="block";
+};
+
+function calculateAge(){
+    var counterElement = document.getElementById("counter");
+    var remainderElement = document.getElementById("remainder");
+    var avgLife = 73;
+    updateAgeDisplay();
+
+    setInterval(updateAgeDisplay, 5);
+ 
+    function updateAgeDisplay(){
+        var currentDate = new Date();
+        var givenDate = new Date(document.getElementById("dob").value);
+
+        var differenceDate = currentDate - givenDate;
+        var millisecondsPerYear = 1000 * 60 * 60 * 24 * 365;
+        var differenceInYears = differenceDate / millisecondsPerYear;
+        document.getElementById("counter").style.display="block";
+        counterElement.textContent = differenceInYears.toFixed(9);
+
+        var timeRemaining = avgLife - differenceInYears;
+        document.getElementById("remainder").style.display="block";
+        remainderElement.textContent = timeRemaining.toFixed(9);
+    };
+
+};
+
+});
